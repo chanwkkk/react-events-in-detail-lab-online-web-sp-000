501ffbca2c3eb0157da8b22a1e85242348018433
Yingqi Chen <chanwk18@hotmail.com>
2020-02-15 19:14:48 +0000

    Automatically backed up by Learn

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -3,7 +3,7 @@
 import React,{Component} from 'react'
 
 class CoordinatesButton extends Component{
-  
+
   render(){
     return (
       <button onClick = {this.props.onReceiveCoordinates}></button>
